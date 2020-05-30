98a42b223b2ae6ad91bb7a7ec841e023accef3b3
Vitaly Khan <swng69@yandex.ru>
2020-05-30 15:20:49 +0800

    Add filter

--- src/main/java/com/vitalykhan/sweater/GreetingController.java
+++ src/main/java/com/vitalykhan/sweater/GreetingController.java
@@ -2,6 +2,7 @@ package com.vitalykhan.sweater;
 
 import com.vitalykhan.sweater.domain.Message;
 import com.vitalykhan.sweater.repository.MessageRepository;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,6 +41,18 @@ public class GreetingController {
         Message message = new Message(text, tag);
         repository.save(message);
         Iterable<Message> messages = repository.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (Strings.isBlank(filter)) {
+            messages = repository.findAll();
+        } else {
+            messages = repository.findByTag(filter);
+        }
         model.put("messages", messages);
         return "main";
     }
--- src/main/java/com/vitalykhan/sweater/repository/MessageRepository.java
+++ src/main/java/com/vitalykhan/sweater/repository/MessageRepository.java
@@ -3,6 +3,10 @@ package com.vitalykhan.sweater.repository;
 import com.vitalykhan.sweater.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
 
 }
