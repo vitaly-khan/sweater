396c3c04c861b6b09bdb9546f977171905024d0c
Vitaly Khan <swng69@yandex.ru>
2020-06-02 06:36:56 +0800

    Add ADMIN role

--- src/main/java/com/vitalykhan/sweater/domain/Role.java
+++ src/main/java/com/vitalykhan/sweater/domain/Role.java
@@ -3,7 +3,7 @@ package com.vitalykhan.sweater.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    ADMIN, USER;
 
     @Override
     public String getAuthority() {
